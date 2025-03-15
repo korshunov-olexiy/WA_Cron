c58cc5fab8d6982b689bf6e1c4b52bb1460ea572
wrk-ntbk <wrk-ntbk@outlook.com>
2025-03-15 12:04:45 +0200

    fix-4

--- wa_cron.ts
+++ wa_cron.ts
@@ -123,6 +123,9 @@ class WhatsAppBot {
       process.stdout.clearLine(0);
       process.stdout.cursorTo(0);
       process.stdout.write(countdownText);
+      if (diffMs <= 0) {
+        process.stdout.write('\n');
+      }
     }, 1000);
   }
 }
