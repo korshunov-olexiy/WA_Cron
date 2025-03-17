ebf2134858a736e3e5ea2b0eead3e2215675b3ad
korshunov-olexiy <korshunov.olexiy@gmail.com>
2025-03-17 12:38:21 +0200

    fix-15

--- wa_cron.ts
+++ wa_cron.ts
@@ -1,7 +1,7 @@
 import { Boom } from '@hapi/boom';
-import makeWASocket, { Browsers, DisconnectReason, useMultiFileAuthState, WASocket } from '@whiskeysockets/baileys';
+import makeWASocket, { DisconnectReason, Browsers, useMultiFileAuthState, WASocket } from '@whiskeysockets/baileys';
 import cron from 'node-cron';
-import * as fs from 'fs';
+import * as fs from 'fs/promises';
 import * as path from 'path';
 import pino from 'pino';
 
@@ -20,50 +20,52 @@ interface Config {
 class WhatsAppBot {
   private sock: WASocket | null = null;
   private configPath: string;
-  private config: Config;
+  private config?: Config;
 
   constructor(configPath: string) {
     this.configPath = path.resolve(__dirname, configPath);
-    this.config = this.readConfig();
+    this.config = undefined;
   }
 
-  private readConfig(): Config {
-    if (!fs.existsSync(this.configPath)) {
-      throw new Error("Файл конфігурації не знайдено.");
+  private async readConfig(): Promise<Config> {
+    try {
+      const data = await fs.readFile(this.configPath, 'utf-8');
+      return JSON.parse(data);
+    } catch {
+      throw new Error("Файл конфігурації не знайдено або пошкоджено.");
     }
-    return JSON.parse(fs.readFileSync(this.configPath, 'utf-8'));
   }
 
-  private saveConfig(): void {
-    fs.writeFileSync(this.configPath, JSON.stringify(this.config, null, 2), 'utf-8');
+  private async saveConfig(): Promise<void> {
+    await fs.writeFile(this.configPath, JSON.stringify(this.config, null, 2), 'utf-8');
   }
 
-  private isSentTime(): boolean {
+  private async isSentTime(): Promise<boolean> {
+    this.config = await this.readConfig();
     const [hour, minute] = this.config.sendTime.split(':').map(Number);
-    const now = new Date();
-    const scheduledTime = new Date(now);
+    const currentDate = new Date();
+    const scheduledTime = new Date(currentDate);
     scheduledTime.setHours(hour, minute, 0, 0);
-    this.config = this.readConfig();
     return (
       !this.config.msgSentToday &&
-      now >= scheduledTime &&
-      now <= new Date(scheduledTime.getTime() + 10 * 60000) // 10 хвилин
+      currentDate >= scheduledTime &&
+      currentDate <= new Date(scheduledTime.getTime() + 10 * 60000)
     );
   }
 
-  async initialize(): Promise<void> {
+  async initialize() {
     const { state, saveCreds } = await useMultiFileAuthState('auth_info');
     this.sock = makeWASocket({
       auth: state,
       logger: pino({ level: 'silent' }),
-      browser: Browsers.baileys(this.config.app_name),
+      browser: Browsers.baileys((await this.readConfig()).app_name),
       printQRInTerminal: true,
       keepAliveIntervalMs: 60000,
     });
     this.sock.ev.on('connection.update', async (update) => {
       const { connection, lastDisconnect, qr } = update;
       if (qr) {
-        console.log(`${this.config.highlightStart}Відскануйте QR-код для авторизації:${this.config.highlightEnd}\n`, qr);
+        console.log(`${this.config!.highlightStart}Відскануйте QR-код для авторизації:${this.config!.highlightEnd}\n${qr}`);
       }
       if (connection === 'close') {
         const shouldReconnect =
@@ -71,62 +73,56 @@ class WhatsAppBot {
         if (shouldReconnect) {
           await this.initialize();
         } else {
-          console.error(`${this.config.errorHighlightStart}Авторизацію не виконано. Завершення роботи.${this.config.errorHighlightEnd}`);
+          console.error(`${this.config!.errorHighlightStart}Авторизацію не виконано. Завершення роботи.${this.config!.errorHighlightEnd}`);
           process.exit(1);
         }
       } else if (connection === 'open') {
-        if (this.isSentTime()) {
-          await this.trySendMessage();
+        if (await this.isSentTime()) {
+          await this.sendMessage();
         }
         this.scheduleMessage();
       }
     });
+
     this.sock.ev.on('creds.update', saveCreds);
-    cron.schedule('0 0 * * *', () => {
+    cron.schedule('0 0 * * *', async () => {
+      this.config = await this.readConfig();
       this.config.msgSentToday = false;
-      this.saveConfig();
+      await this.saveConfig();
     });
   }
 
-  private scheduleMessage(): void {
-    const [hour, minute] = this.config.sendTime.split(':');
+  private scheduleMessage() {
+    const [hour, minute] = this.config!.sendTime.split(':');
     cron.schedule(`${minute} ${hour} * * *`, async () => {
-      await this.trySendMessage();
+      await this.sendMessage();
     });
     this.showCountdown(hour, minute);
   }
 
-  private async trySendMessage(): Promise<void> {
-    while (this.isSentTime()) {
-      const sent = await this.sendMessage();
-      if (sent) {
+  private async sendMessage() {
+    if (!this.sock) return;
+    while (await this.isSentTime()) {
+      try {
+        const groups = await this.sock.groupFetchAllParticipating();
+        const groupMetadata = Object.values(groups).find((group) => group.subject === this.config!.group);
+        if (!groupMetadata) {
+          console.error(`${this.config!.errorHighlightStart}Група "${this.config!.group}" не знайдена.${this.config!.errorHighlightEnd}`);
+          return;
+        }
+        await this.sock.sendMessage(groupMetadata.id, { text: this.config!.message });
+        this.config!.msgSentToday = true;
+        await this.saveConfig();
+        console.log(`\n${this.config!.highlightStart}Повідомлення відправлене у "${this.config!.group}".${this.config!.highlightEnd}`);
         break;
+      } catch (error) {
+        console.error(`${this.config!.errorHighlightStart}Помилка відправки. Повторна спроба...${this.config!.errorHighlightEnd}`);
+        await new Promise((resolve) => setTimeout(resolve, 15000)); // пауза перед повторною спробою
       }
-      await new Promise(r => setTimeout(r, 60000)); // повторити спробу через 1 хв.
     }
   }
 
-  private async sendMessage(): Promise<boolean> {
-    if (!this.sock) return false;
-    try {
-      const groups = await this.sock.groupFetchAllParticipating();
-      const groupMetadata = Object.values(groups).find(g => g.subject === this.config.group);
-      if (!groupMetadata) {
-        console.error(`${this.config.errorHighlightStart}Група "${this.config.group}" не знайдена.${this.config.errorHighlightEnd}`);
-        return false;
-      }
-      await this.sock.sendMessage(groupMetadata.id, { text: this.config.message });
-      this.config.msgSentToday = true;
-      this.saveConfig();
-      console.log(`\n${this.config.highlightStart}Повідомлення відправлене у "${this.config.group}".${this.config.highlightEnd}`);
-      return true;
-    } catch (error) {
-      console.error(`${this.config.errorHighlightStart}Помилка при відправці:${this.config.errorHighlightEnd}`, error);
-      return false;
-    }
-  }
-
-  private showCountdown(hour: string, minute: string): void {
+  private showCountdown(hour: string, minute: string) {
     setInterval(() => {
       const now = new Date();
       const next = new Date();
@@ -138,8 +134,7 @@ class WhatsAppBot {
       const diffSeconds = Math.floor((diffMs % 60000) / 1000);
       process.stdout.clearLine(0);
       process.stdout.cursorTo(0);
-      process.stdout.write(`${this.config.highlightStart}Наступне повідомлення через:${this.config.highlightEnd} ${this.config.errorHighlightStart}${diffHours}год. ${diffMinutes}хв. ${diffSeconds}сек.${this.config.errorHighlightEnd}`);
-      process.stdout.uncork();
+      process.stdout.write(`${this.config!.highlightStart}Наступне повідомлення через:${this.config!.highlightEnd}${this.config!.errorHighlightStart} ${diffHours}год. ${diffMinutes}хв. ${diffSeconds}сек.${this.config!.errorHighlightEnd}`);
     }, 1000);
   }
 }
